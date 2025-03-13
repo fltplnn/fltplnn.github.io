e70c863c7f1006638f60f074ad4561395d064369
fltplnn <hazzavryt@gmail.com>
2025-03-13 17:28:09 +0000

    Create textCenter.js

---
+++ textCenter.js
@@ -0,0 +1,10 @@
+ function adjustTextAlignment() {
+      const input = document.getElementById("icaoInput");
+      if (input.value.length > 0) {
+        input.style.textAlign = "center";
+        input.style.direction = "ltr";
+      } else {
+        input.style.textAlign = "center";
+        input.style.direction = "ltr";
+      }
+    }
