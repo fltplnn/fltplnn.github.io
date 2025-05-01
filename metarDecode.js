ff7db85646c97af1d6c59ecc2638000011f315ce
fltplnn <hazzavryt@gmail.com>
2025-05-01 09:49:39 +0100

    Optimised and added date decoding

--- metarDecode.js
+++ metarDecode.js
@@ -4,16 +4,17 @@ function decodeMetar() {
 
     if (!metar) {
         alert("No METAR data available to decode.");
-        return;
+        return null; // Return null explicitly
     }
 
     const regex = {
         station: /^([A-Z]{4})/,
-        time: /\d{6}Z/,
+        date: /(\d{2})/, // Fixed regex to capture the date
+        time: /(\d{4}Z)/,
         wind: /\b(?<full>(?<direction>\d{3}|VRB)(?<speed>\d{2})(?<gust>G\d{2,3})?(?<units>KT|MPS|KMH|KTS))\b/,
         visibility: /\b((\d{4})|(\d+\s?\d?\/?\d?SM))\b/,
         variableWind: /(\d{3})V(\d{3})/,
-        weather: /(RE|GR|GS|SN|RA|FZ|BR|HZ|FG|TS)/,
+        weather: /(CLR|B|BC|BL|BR|DR|DS|DU|DZ|E|FC|FG|FU|FZ|GR|GS|HZ|IC|MI|PL|PO|PR|PY|RA|SA|SG|SH|SN|SQ|SS|TS|UP|VA|VC)/,
         clouds: /(BKN|SCT|OVC|CLR|FEW)(\d{3})/g,
         temperature: /([M]?\d{2})\/([M]?\d{2})/,
         altimeter: /(Q|A)(\d{4})/
@@ -21,6 +22,19 @@ function decodeMetar() {
 
     const stationMatch = metar.match(regex.station);
     const station = stationMatch ? stationMatch[1] : 'Unknown';
+    
+    const dateMatch = metar.match(regex.date); // Fixed to use regex.date
+    let date = null;
+    
+    if (dateMatch) {
+        const day = dateMatch[1];
+        const suffix = (day === '01' || day === '21' || day === '31') ? 'st' :
+                       (day === '02' || day === '22') ? 'nd' :
+                       (day === '03' || day === '23') ? 'rd' : 'th';
+        date = day + suffix;
+    } else {
+        date = 'Unknown'; // Provide a default value if no match
+    }
 
     const timeMatch = metar.match(regex.time);
     const time = timeMatch ? timeMatch[0] : 'Unknown';
@@ -52,35 +66,35 @@ function decodeMetar() {
     const windVariation = variableWindMatch ? `${variableWindMatch[1]}&deg; to ${variableWindMatch[2]}&deg;` : 'No variable wind reported';
 
     const weatherMatch = metar.match(regex.weather);
-    const weather = weatherMatch ? weatherMatch[0] : 'Clear';
+    let weather = 'No weather detected';
+    
+    if (weatherMatch && weatherMatch[0]) {
+        switch (weatherMatch[0]) {
+            case 'CLR': weather = 'Clear'; break;
+            case 'RA': weather = 'Rain'; break;
+            // Add more cases as needed
+        }
+    }
 
     const cloudsMatch = metar.match(regex.clouds);
-    let clouds = 'No clouds';
+    let clouds = 'No clouds detected';
     if (cloudsMatch) {
         clouds = cloudsMatch.map(layer => {
             const type = layer.substring(0, 3);
             const height = parseInt(layer.substring(3)) * 100;
-            let cloudType = '';
             switch (type) {
-                case 'BKN': cloudType = `Broken ${height}ft`; break;
-                case 'SCT': cloudType = `Scattered ${height}ft`; break;
-                case 'OVC': cloudType = `Overcast ${height}ft`; break;
-                case 'CLR': cloudType = `Clear`; break;
-                case 'FEW': cloudType = `Few ${height}ft`; break;
+                case 'BKN': return `Broken ${height}ft`;
+                case 'SCT': return `Scattered ${height}ft`;
+                case 'OVC': return `Overcast ${height}ft`;
+                case 'CLR': return `Clear`;
+                case 'FEW': return `Few ${height}ft`;
+                default: return '';
             }
-            return cloudType;
         }).join(', ');
     }
 
     const tempMatch = metar.match(regex.temperature);
-
-    const formatTemperature = (temp) => {
-        if (temp[0] === 'M') {
-            return '-' + temp.slice(1);
-        }
-        return temp;
-    };
-
+    const formatTemperature = (temp) => (temp[0] === 'M' ? '-' + temp.slice(1) : temp);
     const temperature = tempMatch ? formatTemperature(tempMatch[1]) + '&deg;C' : 'Unknown';
     const dewPoint = tempMatch ? formatTemperature(tempMatch[2]) + '&deg;C' : 'Unknown';
 
@@ -89,15 +103,12 @@ function decodeMetar() {
     if (altimeterMatch) {
         const type = altimeterMatch[1];
         const value = altimeterMatch[2];
-        if (type === 'Q') {
-            altimeter = value + ' hPa';
-        } else if (type === 'A') {
-            altimeter = parseInt(value) / 100 + ' inHg';
-        }
+        altimeter = type === 'Q' ? value + ' hPa' : parseInt(value) / 100 + ' inHg';
     }
 
     return {
         station,
+        date,
         time,
         wind,
         windVariation,
@@ -110,7 +121,6 @@ function decodeMetar() {
     };
 }
 
-
 function decodeAndDisplayMetar() {
     const decodedMetar = decodeMetar();
 
@@ -120,6 +130,7 @@ function decodeAndDisplayMetar() {
 
     const decodedMetarHtml = `
         <p><strong>Station:</strong> ${decodedMetar.station}</p>
+        <p><strong>Date:</strong> ${decodedMetar.date}</p>
         <p><strong>Time of Observation:</strong> ${decodedMetar.time}</p>
         <p><strong>Wind:</strong> ${decodedMetar.wind.direction} at ${decodedMetar.wind.speed} (${decodedMetar.wind.gust})</p>
         <p><strong>Wind Variation:</strong> ${decodedMetar.windVariation}</p>
